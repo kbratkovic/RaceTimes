a62b7d771a56a18e322b990b9268dd8517b5a764
Kresimir Bratkovic <kbratkovic@gmail.com>
2021-10-19 21:28:54 +0200

    Add authorization to admin controler

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RaceTimes.Models;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace RaceTimes.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private IRaceTimesRepository repository;
@@ -15,6 +17,7 @@ namespace RaceTimes.Controllers
         {
             repository = repo;
         }
+
 
         public IActionResult Index() => 
             View(repository.Racers.OrderBy(h => h.Hours).ThenBy(m => m.Minutes).ThenBy(s => s.Seconds).ThenBy(ms => ms.Milliseconds));
@@ -32,6 +35,7 @@ namespace RaceTimes.Controllers
             return RedirectToAction("Index");
         }
         
+
         public IActionResult DeleteFromList(int? id)
         {
             var obj = UserTimesRepository.UserTimes.FirstOrDefault(r => r.RacerID == id);
@@ -39,6 +43,7 @@ namespace RaceTimes.Controllers
 
             return RedirectToAction("ConfirmTime");
         }
+
 
         public IActionResult ConfirmFromList(int? id)
         {
